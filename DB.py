b581d2ea94aeab4a79dd959234e7fd45d73cda53
gautamch24 <134748442+gautamch24@users.noreply.github.com>
2023-11-27 23:05:11 -0800

    updated db.py

--- DB.py
+++ DB.py
@@ -27,25 +27,23 @@ def create_Table(cursor):
     cursor.execute("SHOW TABLES")
     temp = cursor.fetchall()
     tables = [item[0] for item in temp]
-    if "Customers" not in tables:
+    if "customers" not in tables:
         print(" c does not exist")
 
 def register(cursor,conn,data):
-    CustomerID = random.randint(0,10000)
-
-    hashed_password = hashlib.sha256(data['CUSTOMER_Password'].encode()).hexdigest()
-    
-    cursor.execute(f"""INSERT INTO customers (CUSTOMER_ID, CUSTOMER_Name, CUSTOMER_Password, CUSTOMER_Email, CUSTOMER_Phone_Number, Ticket_ID,Train_ID)
-                        VALUES({CustomerID},'{data ['CUSTOMER_Name']}', '{data ['hashed_password']}', '{data ['CUSTOMER_Email']}', '{data ['CUSTOMER_Phone_Number']}',{0},{0})
+    CustomerID = random.randint(1,10000)
+    cursor.execute(f"""INSERT INTO customers (CUSTOMER_ID, CUSTOMER_Name, CUSTOMER_Password, CUSTOMER_Email, CUSTOMER_Phone_Number)
+                        VALUES({CustomerID},'{data ['CUSTOMER_Name']}', '{data ['CUSTOMER_Password']}', '{data ['CUSTOMER_Email']}', '{data ['CUSTOMER_Phone_Number']}')
                     """)
     conn.commit()
 
 def login(cursor,data):
-    hashed_input_password = hashlib.sha256(data['CUSTOMER_Password'].encode()).hexdigest()
-    cursor.execute(f"""SELECT * FROM customers where CUSTOMER_email ='{data ['CUSTOMER_Email']}' AND CUSTOMER_Password = '{hashed_input_password}'""")
-    if cursor.fetchone() != NONE:
-        return True
-    return False
+    cursor.execute(f"""SELECT * FROM customers where CUSTOMER_email ='{data ['CUSTOMER_Email']}' AND CUSTOMER_Password = '{data ['CUSTOMER_Password']}'""")
+    result = cursor.fetchone()
+    if result != NONE:
+        return result
+    else:
+        return False
 
 def scheduleSelection(cursor,dataForTrain):
     cursor.execute(f"""SELECT * FROM Schedule WHERE Train_Date = '{dataForTrain['Train_Date']}'""")
@@ -93,36 +91,39 @@ def admin(cursor,user,passw):
     if passw:
         db = mysql.connector.connect(host = 'localhost', user = user, password = passw, db = 'RailwayDB')
         cursor = db.cursor()
+        return True
 
 
     else:
         db = mysql.connector.connect(host = 'localhost', user = user, db = 'RailwayDB')
         cursor = db.cursor()
+        return False
 
-    saveqry = "select * from customers"
+    # saveqry = "select * from customers"
 
-    try:
-        cursor.execute(saveqry)
-        result = cursor.fetchall()
+    # try:
+    #     cursor.execute(saveqry)
+    #     result = cursor.fetchall()
 
-        for x in result:
-            print(result)
-        print("qry successful")
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        db.rollback()
+    #     for x in result:
+    #         print(result)
+    #     print("qry successful")
+    # except mysql.connector.Error as err:
+    #     print(f"Error: {err}")
+    #     db.rollback()
         
-    finally:
-        cursor.close()
-        db.close()
+    # finally:
+    #     cursor.close()
+    #     db.close()
 
 
 
 def cancelTicketFunction(cursor,conn,dataForCancel):
+    print(dataForCancel['Ticket_ID'])
     try: 
         cursor.execute(f"""SELECT * FROM Tickets WHERE Ticket_ID = '{dataForCancel['Ticket_ID']}'""")
         ticket = cursor.fetchone()[0]
-        cursor.execute(f"""DELETE FROM customers WHERE Ticket_ID = '{dataForCancel['Ticket_ID']}'""")
+        print(ticket)
         cursor.execute(f"""DELETE FROM Tickets WHERE Ticket_ID = {dataForCancel['Ticket_ID']}""")
         conn.commit()
         return ticket
